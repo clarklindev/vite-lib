e68963dea18b13e1371b47c755e88546ead694fd
clarklindev <clarklindev@gmail.com>
2025-03-29 02:50:50 +0800

    configure separate tsconfig.lib.json to only handle lib/ folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "lib/main.ts"),
+            formats: ["es"],
+        },
+    },
+});
