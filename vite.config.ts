470ee3109d22715a3105e5a5351ad14030879b87
clarklindev <clarklindev@gmail.com>
2025-03-31 02:07:19 +0800

    named package @fightclub/libtest

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from "vite";
-import { extname, relative, resolve } from 'path'
-import { fileURLToPath } from 'node:url'
-import { glob } from 'glob'
-import tailwindcss from '@tailwindcss/vite'
-import dts from 'vite-plugin-dts'
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
+import tailwindcss from "@tailwindcss/vite";
+import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
-import { libInjectCss } from 'vite-plugin-lib-inject-css'
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
     plugins: [
         react(),
         tailwindcss(),
         libInjectCss(),
-        dts({tsconfigPath: './tsconfig.app.json', include: ['lib']})
+        dts({ tsconfigPath: "./tsconfig.app.json", include: ["lib"] }),
     ],
     build: {
         copyPublicDir: false,
@@ -21,26 +21,28 @@ export default defineConfig({
             formats: ["es"],
         },
         rollupOptions: {
-            external: ['react', 'react/jsx-runtime'],
+            external: ["react", "react/jsx-runtime"],
             input: Object.fromEntries(
-                glob.sync('lib/**/*.{ts,tsx}', {
-                    ignore: ["lib/**/*.d.ts"],
-                }).map(file => [
-                    // The name of the entry point
-                    // lib/nested/foo.ts becomes nested/foo
-                    relative(
-                        'lib',
-                        file.slice(0, file.length - extname(file).length)
-                    ),
-                    // The absolute path to the entry file
-                    // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
-                    fileURLToPath(new URL(file, import.meta.url))
-                ])
+                glob
+                    .sync("lib/**/*.{ts,tsx}", {
+                        ignore: ["lib/**/*.d.ts"],
+                    })
+                    .map((file) => [
+                        // The name of the entry point
+                        // lib/nested/foo.ts becomes nested/foo
+                        relative(
+                            "lib",
+                            file.slice(0, file.length - extname(file).length)
+                        ),
+                        // The absolute path to the entry file
+                        // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+                        fileURLToPath(new URL(file, import.meta.url)),
+                    ])
             ),
             output: {
-                assetFileNames: 'assets/[name][extname]',
-                entryFileNames: '[name].js',
-            }
-        }
+                assetFileNames: "assets/[name][extname]",
+                entryFileNames: "[name].js",
+            },
+        },
     },
 });
