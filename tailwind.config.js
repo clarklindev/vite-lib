927bf9d7bc13505323dc4058fa3575e2411a68a1
clarklindev <clarklindev@gmail.com>
2025-04-01 03:00:56 +0800

    tailwind.config.js safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ['./lib/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@fightclub/**/*.{js,ts,jsx,tsx}'],
     theme: {
         extend: {}
     },
     plugins: [],
-    // Important for component libraries
-    corePlugins: {
-        preflight: false // Disable Tailwind's reset
-    },
-    // This ensures your library's styles don't conflict with the consuming app
-    important: true
+    safelist: [
+        {
+            pattern: /^(text|bg|border)-(red|blue|green|yellow|gray|white|black)-(100|200|300|400|500|600|700|800|900)$/
+        }
+    ]
 };
