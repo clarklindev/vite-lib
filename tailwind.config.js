ff487dc44d19f4839635e0069c2c8365049122ad
clarklindev <clarklindev@gmail.com>
2025-04-01 03:07:59 +0800

    set tailwind.config.js purge: false

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@fightclub/**/*.{js,ts,jsx,tsx}'],
+    purge: false,
     theme: {
         extend: {}
     },
-    plugins: [],
-    safelist: [
-        {
-            pattern: /^(text|bg|border)-(red|blue|green|yellow|gray|white|black)-(100|200|300|400|500|600|700|800|900)$/
-        }
-    ]
+    plugins: []
 };
