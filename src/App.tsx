bda6029bbddac92497d45124cb23b94c880f93ff
clarklindev <clarklindev@gmail.com>
2025-04-02 13:37:37 +0800

    Avatar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Button, Card, Input } from '../lib/main'; //after npm build -> This works because your package.json points to the transpiled main file dist/main.ts.
 import './App.css';
 import CardExample from './examples/Card';
 import ButtonExample from './examples/Button';
 import IconExample from './examples/Icon';
 import AccordionExample from './examples/Accordion';
+import AvatarExample from './examples/Avatar';
 
 function App() {
     return (
@@ -13,6 +13,7 @@ function App() {
             <ButtonExample />
             <IconExample />
             <AccordionExample />
+            <AvatarExample />
         </>
     );
 }
