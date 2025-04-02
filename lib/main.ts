cef398bf97a2fbbea9a781ced2e46bd3924ee8d5
clarklindev <clarklindev@gmail.com>
2025-04-02 15:00:54 +0800

    remove exports from main usage from components/

--- lib/main.ts
+++ lib/main.ts
@@ -1,11 +1,2 @@
 // lib/main.ts
 import './styles.css';
-
-export { Button } from './components/Button';
-export { Input } from './components/Input';
-export { Label } from './components/Label';
-
-export { Card } from './components/Card';
-export { Icon } from './components/Icon';
-export { Accordion } from './components/Accordion';
-export { Avatar } from './components/Avatar';
