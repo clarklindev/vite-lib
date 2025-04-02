7c73913945205b8043869ed04f86217b86c60f37
clarklindev <clarklindev@gmail.com>
2025-04-02 08:55:03 +0800

    Component exports to main.ts

--- lib/main.ts
+++ lib/main.ts
@@ -4,4 +4,7 @@ import './styles.css';
 export { Button } from './components/Button';
 export { Input } from './components/Input';
 export { Label } from './components/Label';
+
 export { Card } from './components/Card';
+export { Icon } from './components/Icon';
+export { Accordion } from './components/Accordion';
