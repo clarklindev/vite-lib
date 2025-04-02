cf9cc5baa87f09ef5f4c7b14e9ddab70d96c7fb7
clarklindev <clarklindev@gmail.com>
2025-04-02 15:10:01 +0800

    relative to main, remove exports for components/

--- lib/main.ts
+++ lib/main.ts
@@ -1,2 +1,11 @@
 // lib/main.ts
 import './styles.css';
+
+export { Button } from './components/Button';
+export { Input } from './components/Input';
+export { Label } from './components/Label';
+
+export { Card } from './components/Card';
+export { Icon } from './components/Icon';
+export { Accordion } from './components/Accordion';
+export { Avatar } from './components/Avatar';
