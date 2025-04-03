f1235a9e8d5042323bac2adecc6f7e85b64233c8
clarklindev <clarklindev@gmail.com>
2025-04-03 09:45:03 +0800

    delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './node_modules/@fightclub/**/*.{js,ts,jsx,tsx}'],
-    purge: false,
-    theme: {
-        extend: {}
-    },
-    plugins: []
-};
