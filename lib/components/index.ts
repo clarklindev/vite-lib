0052f2939609ccc243dd7b0f26a36d0898697f96
clarklindev <clarklindev@gmail.com>
2025-04-03 14:11:23 +0800

    package.json exports updated to include deep paths

---
+++ lib/components/index.ts
@@ -0,0 +1,9 @@
+// Export all components
+export { Button } from './Button';
+export { Input } from './Input';
+export { Label } from './Label';
+export { Card } from './Card';
+export { Icon } from './Icon';
+export { Accordion } from './Accordion';
+export { Avatar } from './Avatar';
+export { Badge } from './Badge';
